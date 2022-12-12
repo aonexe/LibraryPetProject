c6d5f723e39ea6b403c078d2ed648ea46a96f3de
Vladislav Vorsin <aonexe@gmail.com>
2022-12-12 23:40:06 +0400

    Person model

--- src/main/java/com/vorsin/library/models/Person.java
+++ src/main/java/com/vorsin/library/models/Person.java
@@ -1,4 +1,31 @@
 package com.vorsin.library.models;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
+
+    @NotEmpty(message = "Name should be not empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
